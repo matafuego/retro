1c0fc20ccffb77ff1f4591990e08fcc255f08c22
matafuego <matafuego@gmail.com>
2017-07-15 20:22:35 -0300

    adding swagger

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const errorHandler = require('api-error-handler');
+var swaggerTools = require('swagger-tools');
+var YAML = require('yamljs');
+var swaggerDoc = YAML.load('openapi.yaml');
 
 
 const app = express();
@@ -11,10 +14,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Require our routes into the application.
 require('./routes')(app);
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
-}));
+
+swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+  // Serve the Swagger documents and Swagger UI
+  app.use(middleware.swaggerUi());
+});
 
 app.use(errorHandler());
+
+
 
 module.exports = app;
